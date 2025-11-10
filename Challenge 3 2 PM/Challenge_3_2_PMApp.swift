41482155b4bc69d9350b04880e4e89748818550b
Anantika <anantika.tiwari09@rafflesgirlssch.edu.sg>
2025-11-10 09:28:17 +0800

    Initial Commit

---
+++ Challenge 3 2 PM/Challenge_3_2_PMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_3_2_PMApp.swift
+//  Challenge 3 2 PM
+//
+//  Created by Anantika Tiwari on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_3_2_PMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge 3 2 PM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge 3 2 PM
+//
+//  Created by Anantika Tiwari on 10/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
