07199e4beefc96848494125c2a2e23107d366955
qin en <qinentoh@gmail.com>
2025-11-10 11:14:08 +0800

    for vidushi

--- Challenge 3 2 PM/PickerView.swift
+++ Challenge 3 2 PM/PickerView.swift
@@ -2,17 +2,42 @@
 //  PickerView.swift
 //  Challenge 3 2 PM
 //
-//  Created by T Krobot on 10/11/25.
+//  Created by swiftinsg on 10/11/25.
 //
-
 import SwiftUI
-
 struct PickerView: View {
+    @State private var bedtime = Date.now
+    @State private var wakeup = Date.now
+    @State private var inputText: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 6) {
+            Text("What time does dawg wanna sleep")
+                .font(.headline)
+        }
+        HStack {
+            DatePicker("", selection: $bedtime, displayedComponents: .hourAndMinute)
+                .labelsHidden()
+                .datePickerStyle(.wheel)
+        }
+        VStack {
+            Text("How long does dawg wanna sleep for")
+                .bold()
+            TextField("Enter your text here", text: $inputText)
+                .textFieldStyle(.roundedBorder)
+        }
+        VStack(alignment: .leading, spacing: 6) {
+            Text("When does dawg wanna wake up")
+                .font(.headline)
+        }
+        HStack {
+            DatePicker("", selection: $wakeup, displayedComponents: .hourAndMinute)
+                .labelsHidden()
+                .datePickerStyle(.wheel)
+        }
     }
 }
-
 #Preview {
     PickerView()
 }
+
+
